9b7a48a3888f61f386d96ce0e3891d9b73f7c738
HoMETE416 <144582981+HoMETE416@users.noreply.github.com>
2023-09-17 21:12:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const streamers = ['vtuber_amiru','failverde','kspksp','nemesisxdfp','hennie2001','earendelxdfp','RUMI0813','shygolone','cygxdfp','iitifox','qttsix','homete416','kannazukilubee','seki_meridian','jongie','ren0809k','listeno_o','v4181'];
+
+function main() {
+    searchStreams();
+}
+
+function searchStreams() {
+    streamers.forEach((streamerName) => {
+        new Twitch.Player('twitch-embed', {
+            width: 560,
+            height: 315,
+            channel: streamerName.trim(),
+            muted: true,
+        });
+    });
+    
+    const iframes = document.querySelectorAll('iframe');
+    for (const iframe of iframes) {
+        iframe.style.margin = '10px';
+        iframe.style.border = 'none';
+    }
+}
